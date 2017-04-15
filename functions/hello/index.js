cf710c0bea192ace0a4a9e18ce8443449d768b13
viveksharma619 <s.vivekblog@gmail.com>
2017-04-16 00:49:23 +0530

    Added querystring params

--- functions/hello/index.js
+++ functions/hello/index.js
@@ -2,17 +2,21 @@ const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient({region: 'us-west-2'});
 
 exports.handle = function(e, ctx, cb) {
-  const name = 'HTML';
-  var params = {
-    TableName: 'interview_questions',
-    limit: 100,
-    ScanIndexForward: true,
-    KeyConditionExpression: "app_name = :name",
-    ExpressionAttributeValues: {
-        ":name": name
+  if(e.name){
+    const name = e.name;
+    var params = {
+      TableName: 'interview_questions',
+      limit: 100,
+      ScanIndexForward: true,
+      KeyConditionExpression: "app_name = :name",
+      ExpressionAttributeValues: {
+          ":name": name
+      }
     }
   }
-
+  else{
+    cb(new Error('Data Incomplete'),null);
+  }
   docClient.query(params, function(err, data){
     if(err){
       cb(err, null);
