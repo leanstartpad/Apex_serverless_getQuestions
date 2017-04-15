66c2965f4e80e74ea4368f17e40cf260495a8642
viveksharma619 <s.vivekblog@gmail.com>
2017-04-16 00:04:42 +0530

    Added query filter for app_name

--- functions/hello/index.js
+++ functions/hello/index.js
@@ -2,12 +2,18 @@ const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient({region: 'us-west-2'});
 
 exports.handle = function(e, ctx, cb) {
+  const name = 'HTML';
   var params = {
     TableName: 'interview_questions',
-    limit: 100
+    limit: 100,
+    ScanIndexForward: true,
+    KeyConditionExpression: "app_name = :name",
+    ExpressionAttributeValues: {
+        ":name": name
+    }
   }
 
-  docClient.scan(params, function(err, data){
+  docClient.query(params, function(err, data){
     if(err){
       cb(err, null);
     }
